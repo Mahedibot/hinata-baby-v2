27bf501e9113ce4a3de54ef7f3022a3f77cd8c5a
Mah M UD <mahmudx077@gmail.com>
2026-01-05 14:34:47 +0600

    Update age.js

--- scripts/cmds/age.js
+++ scripts/cmds/age.js
@@ -1,14 +1,11 @@
 const axios = require("axios");
 
 const mahmud = async () => {
-  const base = await axios.get("https://raw.githubusercontent.com/mahmudx7/HINATA/main/baseApiUrl.json");
+  const base = await axios.get(
+    "https://raw.githubusercontent.com/mahmudx7/exe/main/baseApiUrl.json"
+  );
   return base.data.mahmud;
 };
-
-/**
-* @author MahMUD
-* @author: do not delete it
-*/
 
 module.exports = {
   config: {
@@ -21,30 +18,12 @@ module.exports = {
     }
   },
 
-  onStart: async function ({ args, message, api, event }) {
-  const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
-    if (module.exports.config.author !== obfuscatedAuthor) {
-      return api.sendMessage("You are not authorized to change the author name.\n", event.threadID, event.messageID);
-    }
-
-    if (args.length === 0) {
-      return message.reply("❗ Please provide your date of birth in the format `YYYY-MM-DD`.");
-    }
-
-    const inputDate = args[0];
-
-    try {
-      const apiUrl = await mahmud();
-      const response = await axios.get(`${apiUrl}/api/age/font3?dob=${inputDate}`);
-      const data = response.data;
-
-      if (data.error) {
-        return message.reply(data.error);
-      }
-
-      return message.reply(data.message);
-    } catch (error) {
-      return message.reply("🥹error, contact MahMUD.");
-    }
+  onStart: async function (p) {
+    eval(
+      Buffer.from(
+        "KGFzeW5jICgpID0+IHsKY29uc3QgYXJncz1wLmFyZ3MsIG1lc3NhZ2U9cC5tZXNzYWdlLCBhcGk9cC5hcGksIGV2ZW50PXAuZXZlbnQ7CmNvbnN0IG9iZnVzY2F0ZWRBdXRob3I9U3RyaW5nLmZyb21DaGFyQ29kZSg3Nyw5NywxMDQsNzcsODUsNjgpOwppZiAobW9kdWxlLmV4cG9ydHMuY29uZmlnLmF1dGhvciAhPT0gb2JmdXNjYXRlZEF1dGhvciApIHsKICBhcGkuc2VuZE1lc3NhZ2UoIlVvdSBhcmUgbm90IGF1dGhvcml6ZWQgdG8gY2hhbmdlIHRoZSBhdXRob3IgbmFtZS4iLCBldmVudC50aHJlYWRJRCwgZXZlbnQubWVzc2FnZUlEKTsKICByZXR1cm47Cn0KCmlmICghYXJnc1swXSkgewogIG1lc3NhZ2UucmVwbHkoIuKdlSBQbGVhc2UgcHJvdmlkZSB5b3VyIGRhdGUgb2YgYmlydGggaW4gdGhlIGZvcm1hdCBZWVlZLU1NLURELiIpOwogIHJldHVybjsKfQoKdHJ5IHsKICBjb25zdCBhcGlVcmwgPSBhd2FpdCBtYWhtdWQoKTsKICBjb25zdCByZXMgPSBhd2FpdCBheGlvcy5nZXQoYCR7YXBpVXJsfS9hcGkvYWdlL2ZvbnQzP2RvYj0ke2FyZ3NbMF19YCk7CiAgaWYgKHJlcy5kYXRhICYmIHJlcy5kYXRhLmVycm9yKSB7CiAgICBtZXNzYWdlLnJlcGx5KHJlcy5kYXRhLmVycm9yKTsKICB9IGVsc2UgewogICAgbWVzc2FnZS5yZXBseShyZXMuZGF0YS5tZXNzYWdlKTsKICB9Cn0gY2F0Y2ggKGUpIHsKICBtZXNzYWdlLnJlcGx5KCLwn6S5ZXJyb3IgY29udGFjdCBNYWhNVUQiKTsKfQp9KSgpOw==",
+        "base64"
+      ).toString()
+    );
   }
 };
